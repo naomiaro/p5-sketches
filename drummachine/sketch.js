4126e4fe4476f8f675e23e742d107a3113e3a01e
Naomi Aro <naomiaro@gmail.com>
2022-12-10 23:47:43 -0800

    add crude visual

--- drummachine/sketch.js
+++ drummachine/sketch.js
@@ -5,6 +5,8 @@ Kick01,
 Cymbal,
 OpHat01;
 
+let YELLOW;
+
 function preload() {
   soundFormats('wav');
   CIHat01 = loadSound('CYCdh_ElecK01-ClHat01.wav');
@@ -25,10 +27,95 @@ function setup() {
   text("r - Snr01", 40, 100);
   text("o - Kick01", 40, 120);
   text("p - Cymbal", 40, 140);
+
+  YELLOW = color(255, 204, 0);
+  BLACK = color(0, 0, 0);
+  BLUE = color(0, 0, 255);
+  RED = color(255, 0, 0);
+}
+
+function reset() {
+  let c = color(255, 255, 255);
+  fill(c);
+  noStroke();
+
+  rect(0, 150, windowWidth, 200);
+  stroke(0,0,0);
+}
+
+function drawDrums() {
+  reset();
+
+  fill(YELLOW);
+
+  circle(100, 200, 80);
+  circle(200, 200, 80);
+
+  fill(BLACK);
+
+  circle(100, 300, 80);
+  circle(200, 300, 80);
+
+  fill(BLUE);
+
+  circle(300, 200, 80);
+
+  fill(RED);
+
+  circle(300, 300, 80);
+}
+
+function hitCIHat() {
+  fill(YELLOW);
+  circle(100, 200, 85);
+}
+
+function hitOpHat() {
+  fill(YELLOW);
+  circle(200, 200, 85);
+}
+
+function hitTom() {
+  fill(BLACK);
+  circle(100, 300, 85);
+}
+
+function hitSnr() {
+  fill(BLACK);
+  circle(200, 300, 85);
+}
+
+function hitSymbol() {
+  fill(BLUE);
+  circle(300, 200, 85);
+}
+
+function hitKick() {
+  fill(RED);
+  circle(300, 300, 85);
 }
 
 function draw() {
-  
+  drawDrums();
+
+  if (keyIsDown(81)) {
+    hitCIHat();
+  }
+  if (keyIsDown(87)) {
+    hitOpHat();
+  }
+  if (keyIsDown(69)) {
+    hitTom();
+  }
+  if (keyIsDown(82)) {
+    hitSnr();
+  }
+  if (keyIsDown(79)) {
+    hitKick();
+  }
+  if (keyIsDown(80)) {
+    hitSymbol();
+  }
 }
 
 // https://keyjs.dev/
