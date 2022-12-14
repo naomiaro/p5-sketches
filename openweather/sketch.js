fbaa9196fff3b9e3365dbe850be16c30eb69cd70
Naomi Aro <naomiaro@gmail.com>
2022-12-13 20:30:47 -0800

    copy down main weather

--- openweather/sketch.js
+++ openweather/sketch.js
@@ -1,5 +1,9 @@
 let weatherResponse;
 
+// https://openweathermap.org/weather-conditions
+// Rain, Snow, Clouds, Thunderstorm, Drizzle, Clear
+// Mist, Smoke, Haze, Dust, Fog, Sand, Dust, Ash, Squall, Tornado
+
 // weatherResponse = {"coord":{"lon":-123.3298,"lat":48.4073},"weather":[{"id":804,"main":"Clouds","description":"overcast clouds","icon":"04n"}],"base":"stations","main":{"temp":4.78,"feels_like":3.05,"temp_min":3.33,"temp_max":6,"pressure":1025,"humidity":85},"visibility":10000,"wind":{"speed":2.06,"deg":10},"clouds":{"all":100},"dt":1670988226,"sys":{"type":1,"id":357,"country":"CA","sunrise":1670947008,"sunset":1670977114},"timezone":-28800,"id":6174041,"name":"Victoria","cod":200};
 
 
